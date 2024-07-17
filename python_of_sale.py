fd2ce457f11d6ef5845fdfe50ce3dd62f6c1b2da
Wesley Raymundo <wesleynasray@hotmail.com>
2024-07-16 23:38:30 -0300

    List, add and remove item offers

--- python_of_sale.py
+++ python_of_sale.py
@@ -1,3 +1,4 @@
+import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -5,9 +6,37 @@ from firebase_admin import db
 def main():
     initialize_firebase()
 
-    test = db.reference("/test").get()
-    print(test)
-    
+    menu_option = -1
+    item_offers_ref = db.reference("item-offers")
+
+    while menu_option != 0:
+        print()
+        print("1 - List item offers")
+        print("2 - Add item offer")
+        print("3 - Remove item offer")
+        print("0 - Exit program")
+        menu_option = int(input("Type desired option: "))
+        print()
+
+        if menu_option == 1:
+            offers_dict = item_offers_ref.get()
+            if offers_dict:
+                for item, price in offers_dict.items():
+                    print(f"- {item}: $ {price:.2f}")
+            else:
+                print(offers_dict)
+
+        elif menu_option == 2:
+            new_name = input("Name of the new item: ")
+            new_price = float(input("Price of the new item: "))
+            item_offers_ref.child(new_name).set(new_price)
+            print(f"Added item offer for {new_name} = $ {new_price:.2f}")
+
+        elif menu_option == 3:
+            item_to_delete = input("Name of item offer to remove: ")
+            item_offers_ref.child(item_to_delete).delete()
+            print(f"{item_to_delete} offer deleted")
+
     return
 
 def initialize_firebase():
