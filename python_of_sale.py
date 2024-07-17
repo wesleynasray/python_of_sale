d9bed093067ce7f6112610de1fc7f96d232acc68
Wesley Raymundo <wesleynasray@hotmail.com>
2024-07-16 22:57:20 -0300

    Initialize firebase admin

--- python_of_sale.py
+++ python_of_sale.py
@@ -1,6 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
 def main():
-    print("Hello!")
+    initialize_firebase()
+
+    test = db.reference("/test").get()
+    print(test)
+    
     return
+
+def initialize_firebase():
+    cred = credentials.Certificate("./python-of-sale-firebase-adminsdk-pgjxf-92ea74ad68.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': "https://python-of-sale-default-rtdb.firebaseio.com/"
+    })
 
 if __name__ == "__main__":
     main()
