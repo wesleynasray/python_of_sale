6599225da87f574fdc5615b32324f833dd5f847f
Wesley Raymundo <wesleynasray@hotmail.com>
2024-07-17 22:17:51 -0300

    Refactor variables and functions names

--- python_of_sale.py
+++ python_of_sale.py
@@ -1,4 +1,3 @@
-import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -7,37 +6,51 @@ def main():
     initialize_firebase()
 
     menu_option = -1
-    item_offers_ref = db.reference("item-offers")
+    offers_reference = db.reference("item-offers")
 
     while menu_option != 0:
-        print()
-        print("1 - List item offers")
-        print("2 - Add item offer")
-        print("3 - Remove item offer")
-        print("0 - Exit program")
-        menu_option = int(input("Type desired option: "))
-        print()
+        menu_option = request_menu_option()
 
-        if menu_option == 1:
-            offers_dict = item_offers_ref.get()
-            if offers_dict:
-                for item, price in offers_dict.items():
-                    print(f"- {item}: $ {price:.2f}")
-            else:
-                print(offers_dict)
+        if menu_option == 1: 
+            list_offers(offers_reference)
 
         elif menu_option == 2:
-            new_name = input("Name of the new item: ")
-            new_price = float(input("Price of the new item: "))
-            item_offers_ref.child(new_name).set(new_price)
-            print(f"Added item offer for {new_name} = $ {new_price:.2f}")
+            prompt_add_offer(offers_reference)
 
         elif menu_option == 3:
-            item_to_delete = input("Name of item offer to remove: ")
-            item_offers_ref.child(item_to_delete).delete()
-            print(f"{item_to_delete} offer deleted")
+            prompt_remove_offer(offers_reference)
 
     return
+
+def request_menu_option():
+    print()
+    print("1 - List item offers")
+    print("2 - Add item offer")
+    print("3 - Remove item offer")
+    print("0 - Exit program")
+    menu_option = int(input("Type desired option: "))
+    print()
+    return menu_option
+
+def prompt_remove_offer(offers_reference):
+    item_to_delete = input("Name of item offer to remove: ")
+    offers_reference.child(item_to_delete).delete()
+    print(f"{item_to_delete} offer deleted")
+
+def prompt_add_offer(offers_reference):
+    offer_name = input("Name of the new item: ")
+    offer_price = float(input("Price of the new item: "))
+    offers_reference.child(offer_name).set(offer_price)
+    print(f"Added item offer for {offer_name} = $ {offer_price:.2f}")
+
+def list_offers(offers_reference):
+    offers_dictionary = offers_reference.get()
+    if offers_dictionary:
+        for item, price in offers_dictionary.items():
+            print(f"- {item}: $ {price:.2f}")
+    else:
+        print(offers_dictionary)
+
 
 def initialize_firebase():
     cred = credentials.Certificate("./python-of-sale-firebase-adminsdk-pgjxf-92ea74ad68.json")
