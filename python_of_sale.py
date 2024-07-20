8b1d064eda27524a84a475f522760cb5c8d35aaf
Wesley Raymundo <wesleynasray@hotmail.com>
2024-07-19 23:07:55 -0300

    Add payment registration in database

--- python_of_sale.py
+++ python_of_sale.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import time
 import firebase_admin
 from firebase_admin import credentials
@@ -51,65 +52,94 @@ def main():
             status_name = "ACTIVE" if new_availability == True else "inactive"
             print(f"Event \"{event_name}\" is now {status_name}")
 
+        # CASHIER LOOP
         elif menu_option == 6:
-            cashier_loop()
+            event_name = input("Type event name: ")
+            cashier_loop(event_name)
 
-def cashier_loop():
+def cashier_loop(event_name):
     should_continue = True
-    
     while should_continue:
         print()
         print("=== NEW ORDER ===")
-        order_sentence = input("Enter order sentence \"exit\" (example: \"2 kebab 1 soda 4 water\"): ")
+        order_sentence = input("Enter order sentence or \"exit\" (example: \"2 kebab 1 soda 4 water\"): ")
         
         if order_sentence.strip().lower() == "exit":
             should_continue = False
             return
 
-        order_parts = order_sentence.split()
-        order_dictionary = {}
-        part_index = 0
+        order_dictionary = get_order_dictionary(order_sentence)
+        total_amount = 5
 
-        while part_index < len(order_parts) - 1:
-            name = order_parts[part_index + 1]
-            quantity = int(order_parts[part_index])
+        print_order_review(order_dictionary)
 
-            if name not in order_dictionary:
-                order_dictionary[name] = 0
+        payment_option = request_payment_option()
 
-            order_dictionary[name] += quantity
+        client_name = input("Type client's name: ").title()
 
-            part_index += 2
+        payment_confirmation = request_payment_confirmation()
 
-        print()
-        print("ORDER REVIEW:")
-        for name, quantity in order_dictionary.items():
-            print(f"- {quantity} x {name}")
-
-        print()
-        print("What is the payment method? ")
-        print("1 - Pix")
-        print("2 - Debit Card")
-        print("3 - Credit Card")
-        print("4 - Cash")
-        print("0 - Cancel order")
-        print()
-        payment_option = input("Type desired payment option: ")
-
-        print("Waiting payment operation ", end="", flush="True")
-        for char in "5...4...3...2...1...":
-            print(char, end="", flush=True)
-            time.sleep(.25)
-
-        print(flush=True)
-        finishing_option = ""
-        while finishing_option not in ["yes", "cancel"]:
-            finishing_option = input("Is payment complete? (yes/cancel): ").strip().lower()
-
-        if finishing_option == "cancel":
+        if payment_confirmation == "cancel":
             return
         
-        print("Registered income!")
+        event_name = event_name.strip().upper().replace(" ", "")
+        
+        payment_data = {
+            "datetime": datetime.now().isoformat(),
+            "event": event_name,
+            "method": payment_option,
+            "client": client_name,
+            "amount": total_amount,
+            "items": order_dictionary
+        }
+        db.reference("payments").push(payment_data)
+        print("Payment registered!")
+
+def request_payment_confirmation():
+    print("Waiting payment operation ", end="", flush="True")
+    for char in "5...4...3...2...1...":
+        print(char, end="", flush=True)
+        time.sleep(.25)
+    print()
+
+    finishing_option = ""
+    while finishing_option not in ["yes", "cancel"]:
+        finishing_option = input("Is payment complete? (yes/cancel): ").strip().lower()
+    return finishing_option
+
+def request_payment_option():
+    print()
+    print("What is the payment method? ")
+    print("1 - Pix")
+    print("2 - Debit Card")
+    print("3 - Credit Card")
+    print("4 - Cash")
+    print("0 - Cancel order")
+    print()
+    return input("Type desired payment option: ")
+
+def print_order_review(order_dictionary):
+    print()
+    print("ORDER REVIEW:")
+    for name, quantity in order_dictionary.items():
+        print(f"- {quantity} x {name}")
+
+def get_order_dictionary(order_sentence):
+    order_parts = order_sentence.split()
+    order_dictionary = {}
+    part_index = 0
+
+    while part_index < len(order_parts) - 1:
+        name = order_parts[part_index + 1]
+        quantity = int(order_parts[part_index])
+
+        if name not in order_dictionary:
+            order_dictionary[name] = 0
+
+        order_dictionary[name] += quantity
+
+        part_index += 2
+    return order_dictionary
 
 def request_menu_option():
     print()
